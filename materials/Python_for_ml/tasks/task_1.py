14e94c61564f3a420083f030e0e57a12e597b8fb
Zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2024-10-07 18:48:02 +0300

    I used formatter

--- materials/Python_for_ml/tasks/task_1.py
+++ materials/Python_for_ml/tasks/task_1.py
@@ -1,29 +1,29 @@
-input=input("Enter Your mail: ")
+input = input("Enter Your mail: ")
 input.lower()
-count=0
-if(input.__contains__('@')):
+count = 0
+if (input.__contains__('@')):
     for x in input:
-        if x=='@':
-            count+=1
-    if count!=1:
+        if x == '@':
+            count += 1
+    if count != 1:
         print("Invalid email")
         exit()
-    index=input.index('@')
-    checkpoint=input[index:len(input)]
-    if(not checkpoint.__contains__('.')):
-        print("Invalid email") 
-        exit()  
-    username=input[0:index]
-    domain=input[index+1:input.index('.')]
-    domainEnding=input[len(input)-4:len(input)]
+    index = input.index('@')
+    checkpoint = input[index:len(input)]
+    if (not checkpoint.__contains__('.')):
+        print("Invalid email")
+        exit()
+    username = input[0:index]
+    domain = input[index+1:input.index('.')]
+    domainEnding = input[len(input)-4:len(input)]
     print(username)
     print(domain)
-    if(domainEnding=='.com'):
+    if (domainEnding == '.com'):
         print("Commercial Domain")
-    elif(domainEnding=='.edu'):
+    elif (domainEnding == '.edu'):
         print("Educational Domain")
     else:
         print("Other Domain")
 else:
     print("Invalid email")
-    exit()   
+    exit()
