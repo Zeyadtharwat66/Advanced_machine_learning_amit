c61e67fab7e7183d2e15b9b2d4290feb0a6e2ada
Zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2024-10-07 16:58:50 +0300

    Task_1 Done

--- materials/Python_for_ml/tasks/task_1.py
+++ materials/Python_for_ml/tasks/task_1.py
@@ -1,1 +1,28 @@
-x=20
+input=input("Enter Your mail: ")
+count=0
+if(input.__contains__('@')):
+    for x in input:
+        if x=='@':
+            count+=1
+    if count!=1:
+        print("Invalid email")
+        exit()
+    index=input.index('@')
+    checkpoint=input[index:len(input)]
+    if(not checkpoint.__contains__('.')):
+        print("Invalid email") 
+        exit()  
+    username=input[0:index]
+    domain=input[index+1:input.index('.')]
+    domainEnding=input[len(input)-4:len(input)]
+    print(username)
+    print(domain)
+    if(domainEnding=='.com'):
+        print("Commercial Domain")
+    elif(domainEnding=='.edu'):
+        print("Educational Domain")
+    else:
+        print("Other Domain")
+else:
+    print("Invalid email")
+    exit()   
