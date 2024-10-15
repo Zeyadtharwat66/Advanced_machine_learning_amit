279130f9040af865d6b4b628ba1a9bb46071c98f
Zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2024-10-15 19:52:00 +0300

    modification in the loop paramter

--- materials/Python_for_ml/tasks/task_6.py
+++ materials/Python_for_ml/tasks/task_6.py
@@ -7,7 +7,7 @@ for i in range(1, 21):
     open(os.path.join(directory_path, f"{i}.txt"), "w")
 count = len(os.listdir(directory_path))
 print(count)
-for i in range(10):
+for i in range(11):
     z = random.randint(1, 21)
     if os.path.exists(directory_path + f"\{i}.txt"):
         os.remove(directory_path + f"\{i}.txt")
