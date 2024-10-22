b61199969e98955235cf898cdb05f8378c5f8ade
Zeyad_tharwat60 <Ziadtharwatsoliman@gmail.com>
2024-10-22 17:25:03 +0300

    ChatBot task done

--- materials/Python_for_ml/tasks/task_7.py
+++ materials/Python_for_ml/tasks/task_7.py
@@ -1,3 +1,40 @@
-# class chatbot:
-#     def __init__(self,):
-        
+import random
+
+
+class chatbot:
+    def __init__(self, paths=""):
+        self.paths = paths
+
+    def read_from_location(self):
+        input = open(self.paths, "r")
+        w = input.read().splitlines()
+        return w
+
+    def to_dictionary(self):
+        x = self.read_from_location()
+        dic = {}
+        for i in x:
+            if ":" in i:
+                key = i[0 : i.index(":")]
+                dic[key] = (
+                    i[i.index(":") + 3 : i.index("]")].replace('"', "").split(",")
+                )
+        return dic
+
+    def generate_response(self, question):
+        dic = self.to_dictionary()
+        if question in dic:
+            if question == "bye" or question == "goodbye":
+                print(random.choice(dic[question]))
+                exit()
+            return random.choice(dic[question])
+        else:
+            return "Sorry I do not Understand!"
+
+
+c = chatbot(
+    r"C:\Users\HP\Desktop\Amit\Advanced_machine_learning_amit\materials\Python_for_ml\tasks\responses.txt"
+)
+while True:
+    x = input("Message ChatBot : ")
+    print(c.generate_response(x))
